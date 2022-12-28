cf7317da8deb3bdc6c85ebd62209c7da7326daef
temito93 <temotemo161@gmail.com>
2022-12-29 03:22:52 +0400

    chore: change responsive from lg to md

--- components/Landing/LandingPage.tsx
+++ components/Landing/LandingPage.tsx
@@ -6,24 +6,24 @@ const LandingPage = () => {
   return (
     <Fragment>
       <div className='bg-starting-gradient absolute z-20 top-0 w-screen h-screen' />
-      <div className='h-[30.3125rem] lg:h-[50.5rem] bg-top-gradient w-full flex justify-center items-center flex-col'>
-        <h1 className='text-custom-orange-200 text-2xl lg:text-6xl  text-center relative z-30 leading-9 lg:leading-[5.625rem] font-bold'>
+      <div className='h-[30.3125rem] md:h-[50.5rem] bg-top-gradient w-full flex justify-center items-center flex-col'>
+        <h1 className='text-custom-orange-200 text-2xl md:text-6xl  text-center relative z-30 leading-9 md:leading-[5.625rem] font-bold'>
           Find any quote in <br />
           millions of movie lines
         </h1>
-        <button className='text-white px-[0.875rem] lg:px-[1rem] py-[0.4375rem] lg:py-[0.5625rem]  bg-custom-red-600 rounded mt-8 relative z-30 text-base lg:text-xl font-normal'>
+        <button className='text-white px-[0.875rem] md:px-[1rem] py-[0.4375rem] md:py-[0.5625rem]  bg-custom-red-600 rounded mt-8 relative z-30 text-base md:text-xl font-normal'>
           Get started
         </button>
       </div>
 
       <LandingMovie
-        width='max-w-[19rem] lg:max-w-3xl'
+        width='max-w-[19rem] md:max-w-3xl'
         image='bg-interstellar'
         movie='Interstellar, 2014'
         quote='You have to leave something behind to go forward'
       />
       <LandingMovie
-        width='max-w-[17.063rem] lg:max-w-[62rem]'
+        width='max-w-[17.063rem] md:max-w-[62rem]'
         image='bg-royal '
         movie='The Royal Tenenbaums,2001'
         quote='I think we’re just gonna
@@ -32,7 +32,7 @@ const LandingPage = () => {
         leave it that'
       />
       <LandingMovie
-        width='max-w-[18.32rem] lg:max-w-[62rem]'
+        width='max-w-[18.32rem] md:max-w-[62rem]'
         image='bg-lord'
         movie='The Lord of the Rings, 2003  '
         quote='I see in your eyes the same
