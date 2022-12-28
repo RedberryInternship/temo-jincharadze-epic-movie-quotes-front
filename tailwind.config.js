5d78424bef8de4b1e9c64ec909652cb17bd8283b
temito93 <temotemo161@gmail.com>
2022-12-28 15:55:33 +0400

    feat: add eslint and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("tailwindcss-animation-delay"),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animation-delay'),
   ],
 };
