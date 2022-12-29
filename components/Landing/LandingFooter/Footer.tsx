4c037008ca7c9ff71ad76c773437a103b4cf7c2c
temito93 <temotemo161@gmail.com>
2022-12-29 22:26:42 +0400

    chore: add footer translation

--- components/Landing/LandingFooter/Footer.tsx
+++ components/Landing/LandingFooter/Footer.tsx
@@ -1,7 +1,11 @@
+import { useFooter } from 'components';
+
 const Footer = () => {
+  const { t } = useFooter();
+
   return (
     <div className='bg-footer-gradient h-8 md:h-12 w-full text-custom-orange-200 text-[0.5rem] md:text-xs pl-[2.125rem] md:pl-[4.375rem] flex items-center uppercase font-medium'>
-      © 2022 movie quotes. All rights reserved.
+      {t('landing.copyright')}
     </div>
   );
 };
