ef7815c2693b176a97800b6b6a684e9915a7369a
temito93 <temotemo161@gmail.com>
2022-12-29 22:30:38 +0400

    chore: change footer translation

--- components/Landing/LandingFooter/Footer.tsx
+++ components/Landing/LandingFooter/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <div className='bg-footer-gradient h-8 md:h-12 w-full text-custom-orange-200 text-[0.5rem] md:text-xs pl-[2.125rem] md:pl-[4.375rem] flex items-center uppercase font-medium'>
-      {t('landing.copyright')}
+      © 2022 MOVIE QUOTES. {t('landing.copyright')}
     </div>
   );
 };
