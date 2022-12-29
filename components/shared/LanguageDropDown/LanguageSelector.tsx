d998da11e3541ee2e746f22fb4781fc399e55542
temito93 <temotemo161@gmail.com>
2022-12-29 18:03:38 +0400

    style: language select on hover

--- components/shared/LanguageDropDown/LanguageSelector.tsx
+++ components/shared/LanguageDropDown/LanguageSelector.tsx
@@ -6,14 +6,21 @@ const LanguageSelector = () => {
 
   return (
     <div className='relative' ref={refEl}>
-      <div className='flex items-center' onClick={toggleClickHandler}>
+      <div
+        className='flex items-center cursor-pointer'
+        onClick={toggleClickHandler}
+      >
         <div className='mr-[0.64rem] font-normal text-base'>Eng</div>
         <ArrowDown />
       </div>
       {toggle && (
         <div className='absolute bg-white top-10'>
-          <div className='py-2 text-black px-3 text-base'>ქართული</div>
-          <div className='text-black py-2 px-3 text-base'>English</div>
+          <div className='py-2 text-black px-3 text-base hover:bg-black hover:text-white cursor-pointer'>
+            ქართული
+          </div>
+          <div className='text-black py-2 px-3 text-base hover:bg-black hover:text-white cursor-pointer'>
+            English
+          </div>
         </div>
       )}
     </div>
