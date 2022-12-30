8e029ded7eb45697b8f1bdedae04a6c6cec4006b
temito93 <temotemo161@gmail.com>
2022-12-30 20:33:56 +0400

    chore: add background color for backdrop

--- components/shared/Modal/Backdrop/Backdrop.tsx
+++ components/shared/Modal/Backdrop/Backdrop.tsx
@@ -3,7 +3,7 @@ import { BackDropProps } from './types';
 const Backdrop: React.FC<BackDropProps> = (props) => {
   return (
     <div
-      onClick={props.onClick}
+      onClick={props.onClose}
       className='fixed top-0 left-0 w-full h-screen z-[5] bg-blur-bg'
     />
   );
