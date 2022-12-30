f0ff9d0bfef586333a3de68237c80e8bf6dfe88e
temito93 <temotemo161@gmail.com>
2022-12-30 23:13:08 +0400

    chore add padding to FormWrapper

--- components/Landing/LandingHeader/FormWrapper/FormWrapper.tsx
+++ components/Landing/LandingHeader/FormWrapper/FormWrapper.tsx
@@ -3,7 +3,7 @@ import { FormWrapperProps } from './types';
 
 const FormWrapper: React.FC<FormWrapperProps> = (props) => {
   return (
-    <div className='h-screen w-full fixed bg-custom-zinc-800 z-[6] flex justify-center items-center'>
+    <div className='h-screen w-full fixed bg-custom-zinc-800 z-[6] flex justify-center items-center px-2'>
       <div className='absolute top-8 right-10'>
         <Close />
       </div>
