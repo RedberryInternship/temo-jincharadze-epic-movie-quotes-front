5f58de3daa3bb20595283613247964d87908d99b
temito93 <temotemo161@gmail.com>
2022-12-30 22:58:17 +0400

    chore: add password show/hide icon in Input component

--- components/shared/Input/Input.tsx
+++ components/shared/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { Eye } from 'components';
 import { InputProps } from './types';
 
 const Input: React.FC<InputProps> = (props) => {
@@ -8,7 +9,7 @@ const Input: React.FC<InputProps> = (props) => {
           {props.label} <span className='text-custom-red-500'>*</span>
         </label>
       </div>
-      <div>
+      <div className='relative'>
         <input
           {...props.register}
           type={props.type}
@@ -16,6 +17,11 @@ const Input: React.FC<InputProps> = (props) => {
           placeholder={props.placeholder}
           className='rounded h-[2.4rem] px-2 pl-3 w-full border focus:ring-0 border-custom-gray-300 text-base focus:border-custom-gray-300 focus:outline-none outline-none font-normal'
         />
+        {props.type === 'password' && (
+          <div className='absolute top-[0.8rem] right-[0.9rem] cursor-pointer'>
+            <Eye />
+          </div>
+        )}
       </div>
     </div>
   );
