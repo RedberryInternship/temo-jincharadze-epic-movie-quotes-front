dd50e9ffebfeb6e325152a075afc4df35b1b02d6
temito93 <temotemo161@gmail.com>
2022-12-30 22:35:29 +0400

    chore: add close icon in FormWrapper

--- components/Landing/LandingHeader/FormWrapper/FormWrapper.tsx
+++ components/Landing/LandingHeader/FormWrapper/FormWrapper.tsx
@@ -1,8 +1,12 @@
+import { Close } from 'components';
 import { FormWrapperProps } from './types';
 
 const FormWrapper: React.FC<FormWrapperProps> = (props) => {
   return (
     <div className='h-screen w-full fixed bg-custom-zinc-800 z-[6] flex justify-center items-center'>
+      <div className='absolute top-8 right-10'>
+        <Close />
+      </div>
       {props.children}
     </div>
   );
