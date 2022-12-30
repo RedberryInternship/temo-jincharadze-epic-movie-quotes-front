7909676fe1df251a7d94f979a34729fe27c73a48
temito93 <temotemo161@gmail.com>
2022-12-30 18:45:38 +0400

    chore: check if document exists for modal elements

--- components/shared/Modal/Modal.tsx
+++ components/shared/Modal/Modal.tsx
@@ -3,18 +3,21 @@ import { BackDrop } from 'components';
 import { ModalProps } from './types';
 
 const Modal: React.FC<ModalProps> = (props) => {
-  return (
-    <>
-      {createPortal(
-        <BackDrop onClick={props.onClick} />,
-        document.getElementById('backdrop')!
-      )}
-      {createPortal(
-        <div className='fixed z-[6] text-white'>{props.children}</div>,
-        document.getElementById('modal')!
-      )}
-    </>
-  );
+  if (typeof window === 'object') {
+    return (
+      <>
+        {createPortal(
+          <BackDrop onClick={props.onClick} />,
+          document.getElementById('backdrop')!
+        )}
+        {createPortal(
+          <div className='fixed z-[6] text-white'>{props.children}</div>,
+          document.getElementById('modal')!
+        )}
+      </>
+    );
+  }
+  return null;
 };
 
 export default Modal;
