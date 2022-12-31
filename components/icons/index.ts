8319e9c94a6f1b477062edb1fb4e799d71257e2c
temito93 <temotemo161@gmail.com>
2022-12-31 13:09:15 +0400

    chore: import arrow left icon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as ArrowDown } from './ArrowDown';
 export { default as Google } from './Google';
 export { default as Close } from './Close';
 export { default as Eye } from './Eye';
+export { default as ArrowLeft } from './ArrowLeft';
