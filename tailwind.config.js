49ef20b4c5b0944bfa19a668d6d3f70eee6e6524
temito93 <temotemo161@gmail.com>
2023-01-02 21:54:07 +0400

    chore: change gradient height for starting landing text

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       backgroundImage: {
         'starting-gradient':
-          'linear-gradient(180deg, #000000 10%, rgba(0, 0, 0, 0) 65.21%, rgba(0, 0, 0, 0) 100%)',
+          'linear-gradient(180deg, #000000 55%, rgba(0, 0, 0, 0) 95.21%, rgba(0, 0, 0, 0) 100%)',
         'movies-gradient':
           'linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 55.21%, rgba(0, 0, 0, 0) 100%), linear-gradient(90deg, #000000 0%, rgba(0, 0, 0, 0) 55.21%, rgba(0, 0, 0, 0) 100%)',
         'top-gradient':
