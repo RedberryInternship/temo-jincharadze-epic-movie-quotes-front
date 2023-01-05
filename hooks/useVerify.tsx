e647bd5141daa264c78e4158625582b0df54b49a
temito93 <temotemo161@gmail.com>
2023-01-06 00:11:07 +0400

    feat: add custom hook for email verification

---
+++ hooks/useVerify.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { verifyEmail } from 'services';
+
+const useVerify = () => {
+  const { query } = useRouter();
+  let link: string;
+
+  const checkEmail = async () => {
+    try {
+      let url = new URL(query.verify?.toString()!);
+      let params = new URLSearchParams(url.search);
+      params.append('expires', query.expires as string);
+      params.append('signature', query.signature as string);
+      link = '/verify-account' + '?' + params;
+    } catch (err) {}
+  };
+
+  checkEmail();
+
+  const { data } = useQuery({
+    queryKey: ['verify email'],
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    enabled: !!query.verify && !!query.signature && !!query.expires,
+    queryFn: async () => verifyEmail(link),
+  });
+  return { data };
+};
+
+export default useVerify;
