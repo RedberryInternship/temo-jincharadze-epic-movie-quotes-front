c341f67b15ae32cdddb8d9af8008c7833c749b1d
temito93 <temotemo161@gmail.com>
2023-01-05 21:51:07 +0400

    feat: wrap Component in _app.tsx with  QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default appWithTranslation(MyApp);
