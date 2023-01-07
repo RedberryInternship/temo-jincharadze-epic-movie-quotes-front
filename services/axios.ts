eaf4a3c78c7c16c6b8ffe28283139c8f8289d15e
temito93 <temotemo161@gmail.com>
2023-01-07 18:45:09 +0400

    feat: add withCredentials in axios instance

--- services/axios.ts
+++ services/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
   headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
 export default instance;
