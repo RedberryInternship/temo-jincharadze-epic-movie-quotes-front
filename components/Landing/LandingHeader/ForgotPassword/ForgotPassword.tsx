9bdc88fa06e8312829abbb8c0caf45c74fd9ba8e
temito93 <temotemo161@gmail.com>
2023-01-09 02:33:30 +0400

    feat: add forgot password modal with custom hook

--- components/Landing/LandingHeader/ForgotPassword/ForgotPassword.tsx
+++ components/Landing/LandingHeader/ForgotPassword/ForgotPassword.tsx
@@ -1,32 +1,67 @@
-import { ArrowLeft, Button, Input, useForgotPassword } from 'components';
+import { ErrorMessage } from '@hookform/error-message';
+import {
+  ArrowLeft,
+  Button,
+  Input,
+  Message,
+  useForgotPassword,
+} from 'components';
+import { FormProvider } from 'react-hook-form';
 import { ForgotPasswordTypes } from './types';
 
 const ForgotPassword: React.FC<ForgotPasswordTypes> = (props) => {
-  const { t } = useForgotPassword();
+  const { t, register, errors, form, checkEmailHandler, isLoading } =
+    useForgotPassword();
+
   return (
     <div className='w-[22.5rem]'>
       <div className='mb-8 text-center'>
-        <h2 className='text-white font-medium text-2xl'>
+        <h2 className='text-white font-medium text-3.5'>
           {t('forgotPassword.about')}
         </h2>
-        <p className='text-custom-gray-500 text-xs font-normal mt-3'>
+        <p className='text-custom-gray-500 text-base font-normal mt-3'>
           {t('forgotPassword.description')}
         </p>
       </div>
       <div className='w-full'>
-        <form>
-          <Input
-            containerClass='mt-8'
-            label={t('forgotPassword.email')}
-            type='email'
-            name='email'
-            placeholder={t('forgotPassword.emailPlaceholder')!}
-          />
+        <FormProvider {...form}>
+          <form>
+            <Input
+              register={register('email', {
+                required: { value: true, message: t('errors.required') },
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$/,
+                  message: t('errors.emailPattern'),
+                },
+              })}
+              containerClass='mt-8'
+              label={t('forgotPassword.email')}
+              type='email'
+              name='email'
+              placeholder={t('forgotPassword.emailPlaceholder')!}
+            />
 
-          <Button className='bg-custom-red-600 hover:bg-custom-red-700 w-full text-white text-center h-[2.4rem] mt-10 rounded'>
-            {t('forgotPassword.btn')}
-          </Button>
-        </form>
+            <div className='h-7 mt-1'>
+              <ErrorMessage
+                name='email'
+                errors={errors}
+                render={({ message }) => <Message message={message} />}
+              />
+            </div>
+
+            <Button
+              className={`${
+                isLoading
+                  ? 'bg-custom-rose-500'
+                  : 'bg-custom-red-600 hover:bg-custom-red-700'
+              } w-full text-white text-center h-[2.4rem] mt-3 rounded`}
+              onClick={checkEmailHandler}
+              disabled={isLoading}
+            >
+              {t('forgotPassword.btn')}
+            </Button>
+          </form>
+        </FormProvider>
         <div
           className='mt-6 flex justify-center items-center cursor-pointer'
           onClick={props.backToLoginClick}
--- components/Landing/LandingHeader/ForgotPassword/useForgotPassword.tsx
+++ components/Landing/LandingHeader/ForgotPassword/useForgotPassword.tsx
@@ -1,8 +1,63 @@
 import { useTranslation } from 'next-i18next';
+import { useState } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { checkPasswordResetEmail } from 'services';
+import { showModalActions } from 'store';
 
 const useForgotPassword = () => {
-  const { t } = useTranslation('forms');
-  return { t };
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  const form = useForm<{ email: string }>({
+    mode: 'all',
+    defaultValues: { email: '' },
+  });
+
+  const {
+    control,
+    register,
+    getValues,
+    setError,
+    formState: { errors, isValid },
+  } = form;
+  const { t, i18n } = useTranslation('forms');
+
+  const email = useWatch({ control: control });
+
+  const checkEmailHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (isValid) {
+      const values = getValues();
+
+      const newFormData = {
+        email: values.email,
+        lang: i18n.language,
+      };
+      try {
+        setIsLoading(true);
+        const response = await checkPasswordResetEmail(newFormData);
+        setIsLoading(false);
+        dispatch(showModalActions.setModalValue('password reset sent'));
+      } catch (err: any) {
+        setIsLoading(false);
+        if (err.response.data.errors.email) {
+          setError('email', {
+            message: t('exists.email')!,
+          });
+        }
+        if (
+          err.response.data.message === 'Your account email is not verified'
+        ) {
+          setError('email', {
+            message: t('verify.email')!,
+          });
+        }
+      }
+    }
+  };
+
+  return { t, register, errors, form, checkEmailHandler, isLoading };
 };
 
 export default useForgotPassword;
