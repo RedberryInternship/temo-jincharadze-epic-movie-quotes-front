26844699c47dcab2c2482924702c101a04b01f2c
temito93 <temotemo161@gmail.com>
2023-01-08 16:25:26 +0400

    style: change width of lendgin page movies

--- components/Landing/LandingMovies/LandingMovie.tsx
+++ components/Landing/LandingMovies/LandingMovie.tsx
@@ -13,11 +13,11 @@ const LandingMovie: React.FC<PropsTypes> = (props) => {
           <div className='w-4 md:w-[3.3rem] bg-white h-s mt-4 md:mt-9 mr-2 md:mr-4' />
           <div className='flex flex-col'>
             <h1
-              className={`max-w-[19rem] font-bold w-full text-xl md:text-3xl lg:text-5.5 leading-8 md:leading-[4.6rem]  ${props.width}`}
+              className={`max-w-[19rem] font-bold w-full text-xl md:text-5.5 leading-8 md:leading-[4.6rem]  ${props.width}`}
             >
               “{props.quote}”
             </h1>
-            <h1 className='mt-3 md:mt-4 text-base md:text-2xl lg:text-3xl  text-custom-zinc-300 font-bold'>
+            <h1 className='mt-3 md:mt-4 text-base md:text-3xl  text-custom-zinc-300 font-bold'>
               {props.movie}
             </h1>
           </div>
--- components/Landing/LandingPage.tsx
+++ components/Landing/LandingPage.tsx
@@ -90,18 +90,18 @@ const LandingPage = () => {
         onLoginClick={onLoginHandler}
       />
       <div className='h-[30.3125rem] md:h-[50.5rem]  bg-top-gradient w-full flex justify-center items-center flex-col'>
-        <h1 className='text-custom-orange-200 text-2xl md:text-6xl  text-center relative z-[3] leading-9 md:leading-[5.6rem] font-bold max-w-[20rem] md:max-w-[50rem]'>
+        <h1 className='text-custom-orange-200 text-2xl md:text-6xl  text-center relative z-[3] leading-9 md:leading-[5.6rem] font-bold max-w-[17rem] md:max-w-[42rem]'>
           {t('landing.about')}
         </h1>
         <Button
-          className='px-[0.9rem] md:px-4 py-[0.4rem] md:py-[0.5rem] bg-custom-red-600 hover:bg-custom-red-700 rounded mt-8 relative z-[3] md:text-xl'
+          className='px-[0.9rem] md:px-4 py-[0.4rem] md:py-[0.5rem] bg-custom-red-600 hover:bg-custom-red-700 rounded mt-8 relative z-[3] text-base  md:text-xl'
           onClick={onSignUpHandler}
         >
           {t('landing.getStarted')}
         </Button>
       </div>
       <LandingMovie
-        width='max-w-[25rem] md:max-w-4xl'
+        width='max-w-[19rem] md:max-w-3xl'
         image='bg-interstellar'
         movie={t('landing.interstellarMovie.name')}
         quote={t('landing.interstellarMovie.quote')}
