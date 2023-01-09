1a5ab8ace39361f48c94727b2e2164d894370aef
temito93 <temotemo161@gmail.com>
2023-01-09 20:28:55 +0400

    chore: pass props on Message component

--- components/shared/Error/Message.tsx
+++ components/shared/Error/Message.tsx
@@ -1,7 +1,7 @@
-const Message: React.FC<{ message: string }> = ({ message }) => {
+const Message: React.FC<{ message: string }> = (props) => {
   return (
     <p className='text-sm font-normal text-red-500 pl-1 whitespace-nowrap'>
-      {message}
+      {props.message}
     </p>
   );
 };
