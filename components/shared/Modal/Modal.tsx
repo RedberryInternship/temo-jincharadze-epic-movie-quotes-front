b31dc50dc3c799d02615ef50b05cb61e04e6b14e
temito93 <temotemo161@gmail.com>
2023-01-09 20:27:41 +0400

    chore add ternary check on Modal

--- components/shared/Modal/Modal.tsx
+++ components/shared/Modal/Modal.tsx
@@ -3,21 +3,20 @@ import { BackDrop } from 'components';
 import { ModalProps } from './types';
 
 const Modal: React.FC<ModalProps> = (props) => {
-  if (typeof window === 'object') {
-    return (
-      <>
-        {createPortal(
-          <BackDrop onClose={props.onClose} />,
-          document.getElementById('backdrop')!
-        )}
-        {createPortal(
-          <div className='fixed z-[6] text-white'>{props.children}</div>,
-          document.getElementById('modal')!
-        )}
-      </>
-    );
-  }
-  return null;
+  return typeof window === 'object' ? (
+    <>
+      (
+      {createPortal(
+        <BackDrop onClose={props.onClose} />,
+        document.getElementById('backdrop')!
+      )}
+      {createPortal(
+        <div className='fixed z-[6] text-white'>{props.children}</div>,
+        document.getElementById('modal')!
+      )}
+      )
+    </>
+  ) : null;
 };
 
 export default Modal;
