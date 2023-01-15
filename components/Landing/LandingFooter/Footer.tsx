585797f8a1024e5ecd13440f2dbe71c28e41351b
temito93 <temotemo161@gmail.com>
2023-01-15 16:02:27 +0400

    chore: add relative and z index for footer

--- components/Landing/LandingFooter/Footer.tsx
+++ components/Landing/LandingFooter/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const { t } = useFooter();
 
   return (
-    <div className='bg-footer-gradient h-8 md:h-12 w-full text-custom-orange-200 text-s  md:text-xs pl-8 md:pl-[4.4rem] flex items-center uppercase font-medium'>
+    <div className='bg-footer-gradient h-8  relative z-[3] md:h-12 w-full text-custom-orange-200 text-s  md:text-xs pl-8 md:pl-[4.4rem] flex items-center uppercase font-medium'>
       © 2022 MOVIE QUOTES. {t('landing.copyright')}
     </div>
   );
