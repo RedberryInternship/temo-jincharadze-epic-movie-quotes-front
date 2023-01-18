584190cd29df6c20b1c06b382dde6fdcd7e2aa93
temito93 <temotemo161@gmail.com>
2023-01-18 12:54:54 +0400

    chore: add index export for useGetUserData hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useVerify } from './useVerify';
 export { default as useNotificationLang } from './useNotificationLang';
 export { default as usePasswordVerify } from './usePasswordVerify';
+export { default as useGetUserData } from './useGetUserData';
