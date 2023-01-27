f2d215d05eb33470025dbea4d436671272226ffa
temito93 <temotemo161@gmail.com>
2023-01-27 15:13:48 +0400

    feat: add midleware for checking routes

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { AUTH_ROUTES, GUEST_ROUTES } from 'config';
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  let response = NextResponse.next();
+  const cookies = request.cookies.getAll();
+  const isAuth = cookies.some((cookie) => cookie.name === 'isAuth');
+  const { pathname } = request.nextUrl;
+
+  if (pathname === GUEST_ROUTES && isAuth) {
+    response = NextResponse.rewrite(new URL('/movie-list', request.url));
+  }
+
+  for (const auth of AUTH_ROUTES) {
+    if (pathname.includes(auth) && !isAuth) {
+      response = NextResponse.rewrite(new URL('/', request.url));
+    }
+  }
+
+  return response;
+}
