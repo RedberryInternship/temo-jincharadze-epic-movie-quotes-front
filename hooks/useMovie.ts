c5996a38649cf15dd5034827573047b050b758ea
temito93 <temotemo161@gmail.com>
2023-01-27 15:15:31 +0400

    feat: add custom hook for movies

---
+++ hooks/useMovie.ts
@@ -0,0 +1,52 @@
+import { useGetUserData } from 'hooks';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { useQuery, useQueryClient } from 'react-query';
+import { getUserMovies } from 'services';
+
+const useMovie = () => {
+  const { isFocused, setIsFocused } = useGetUserData();
+  const { i18n, t } = useTranslation('forms');
+  const { replace, query } = useRouter();
+
+  const form = useForm<{ search: string }>({
+    mode: 'all',
+    defaultValues: {
+      search: '',
+    },
+  });
+
+  const { setValue } = form;
+
+  const { data } = useQuery({
+    queryKey: ['user movies', query],
+    queryFn: () => getUserMovies(query),
+    retry: 0,
+    refetchOnWindowFocus: false,
+  });
+  const { register, handleSubmit } = form;
+
+  const queryClient = useQueryClient();
+
+  const handleSearch = (data: { search: string }) => {
+    replace({ query: data });
+    queryClient.invalidateQueries({ queryKey: 'user movies' });
+    setValue('search', '');
+  };
+
+  return {
+    isFocused,
+    setIsFocused,
+    query,
+    data,
+    i18n,
+    form,
+    register,
+    handleSubmit,
+    t,
+    handleSearch,
+  };
+};
+
+export default useMovie;
