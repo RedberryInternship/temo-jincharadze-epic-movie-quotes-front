f97b2e1672b324f962e1665ad2ad609d0e0fb468
temito93 <temotemo161@gmail.com>
2023-01-27 15:24:14 +0400

    chore: add translations for dashboard

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'http://127.0.0.1:8000'],
   },
   i18n,
   reactStrictMode: true,
