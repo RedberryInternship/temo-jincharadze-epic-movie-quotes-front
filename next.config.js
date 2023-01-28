d2143fe16e4ed8aefeb4ace63853b7b3e6368ac5
temito93 <temotemo161@gmail.com>
2023-01-28 18:07:01 +0400

    chore: add env for google image storage

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'http://127.0.0.1:8000'],
+    domains: process.env.DOMAINS,
   },
   i18n,
   reactStrictMode: true,
