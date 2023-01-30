39eb8c1f57d58584ff71782435cae7b9d3fdf4ba
temito93 <temotemo161@gmail.com>
2023-01-30 16:53:08 +0400

    chore: add index exports

--- services/Quote/index.ts
+++ services/Quote/index.ts
@@ -2,3 +2,4 @@ export { default as storeQuote } from './storeQuote';
 export { default as deleteQuote } from './deleteQuote';
 export { default as getQuote } from './getQuote';
 export { default as updatedQuote } from './updateQuote';
+export { default as allQuotes } from './allQuotes';
