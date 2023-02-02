9c702b9989c251baad45c01fde6b0741f8f3e283
temito93 <temotemo161@gmail.com>
2023-02-02 15:53:15 +0400

    feat: add custom hook for notifications

--- components/shared/Auth/Navigation/useNavigation.tsx
+++ components/shared/Auth/Navigation/useNavigation.tsx
@@ -1,19 +1,33 @@
 import { deleteCookie } from 'cookies-next';
+import { useNotification } from 'hooks';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 import { useDispatch } from 'react-redux';
-import { logout } from 'services';
+import { logout, readNotification } from 'services';
 import { showPanelActions } from 'store';
 
 const useNavigation = () => {
+  const { data: userNotification } = useNotification();
+
+  const [isNotificationOpen, setIsNotificationOpen] = useState<boolean>(false);
   const dispatch = useDispatch();
-  const { replace, pathname } = useRouter();
+  const { replace, pathname, push } = useRouter();
 
   const { t } = useTranslation('forms');
 
   const panelToggleHandler = () => {
     dispatch(showPanelActions.setPanel(true));
   };
+
+  const notificationToggleHandler = () => {
+    setIsNotificationOpen((prev) => !prev);
+  };
+
+  const filterHasNew = userNotification?.data.filter(
+    (notification: { has_new: number }) => notification.has_new === 1
+  );
 
   const logoutHandler = async () => {
     try {
@@ -24,7 +38,32 @@ const useNavigation = () => {
     } catch (error: any) {}
   };
 
-  return { panelToggleHandler, logoutHandler, t, pathname };
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(readNotification);
+
+  const readHandler = (quoteId: number, movieId: number) => {
+    mutate({ quote_id: quoteId });
+    push(`/movie-list/${movieId}?show=view-quote&id=${quoteId}`);
+  };
+
+  const markAllReadHandler = () => {
+    mutate({ markAllAsRead: true });
+    queryClient.invalidateQueries('user notifications');
+  };
+
+  return {
+    panelToggleHandler,
+    logoutHandler,
+    t,
+    pathname,
+    notificationToggleHandler,
+    isNotificationOpen,
+    userNotification,
+    readHandler,
+    filterHasNew,
+    markAllReadHandler,
+  };
 };
 
 export default useNavigation;
