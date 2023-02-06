ad8b3e83bb70c80dda8744a1d7f84238a3f2c637
temito93 <temotemo161@gmail.com>
2023-02-07 00:35:14 +0400

    feat: add gmail profile with custom hook

---
+++ components/Profile/Google/GoogleProfile.tsx
@@ -0,0 +1,261 @@
+import { ErrorMessage } from '@hookform/error-message';
+import {
+  ArrowLeft,
+  Button,
+  CloseSuccess,
+  Dashboard,
+  Message,
+  ProfileImageUpload,
+  ProfileInput,
+  Success,
+  useGoogleProfile,
+} from 'components';
+import Link from 'next/link';
+import { FormProvider } from 'react-hook-form';
+
+const GoogleProfile = () => {
+  const {
+    name,
+    emails,
+    isOpen,
+    setIsOpen,
+    form,
+    usernameOptions,
+    errors,
+    closeAddUser,
+    checkUsernameHandler,
+    isPromptOpen,
+    closePrompt,
+    confirmPrompt,
+    getValues,
+    clearInputs,
+    saveChanges,
+    updateProfile,
+    handleSubmit,
+    showSuccess,
+    closeShowSuccessHandler,
+    disabledInput,
+    disabledHandler,
+  } = useGoogleProfile();
+
+  return (
+    <Dashboard>
+      <div className='relative'>
+        <div className='py-6 pl-10 w-max lg:hidden'>
+          <Link href='/news-feed'>
+            <ArrowLeft />
+          </Link>
+        </div>
+
+        {showSuccess && (
+          <>
+            <div
+              className='bg-dashboard-color opacity-60 absolute w-full h-screen z-[10] top-0'
+              onClick={closeShowSuccessHandler}
+            />
+            <div className='px-8 absolute top-10 w-full z-[11] lg:right-12 lg:w-max'>
+              <div className='bg-success-bg border border-solid rounded border-custom-slate-300'>
+                <div className='flex items-center relative p-4'>
+                  <Success />
+                  <p className='font-normal text-base text-custom-green-900 ml-2 lg:pr-10'>
+                    Changes updated succsessfully
+                  </p>
+                  <div
+                    className='absolute right-5 top-5 cursor-pointer'
+                    onClick={closeShowSuccessHandler}
+                  >
+                    <CloseSuccess />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        <div className='lg:mt-8 lg:max-w-[62.5rem] lg:w-full lg:mx-auto lg:px-3'>
+          <FormProvider {...form}>
+            <form
+              onSubmit={handleSubmit(updateProfile)}
+              className='lg:flex lg:justify-center lg:flex-col md:pr-4'
+            >
+              <div className='hidden lg:block text-white font-medium text-2xl mb-32 lg:ml-12'>
+                My profile
+              </div>
+              <div
+                className={`bg-header-rgba lg:bg-custom-neutral-900 max-w-[62.5rem] w-full h-max pb-20 rounded-xl ${
+                  isOpen ? 'hidden lg:block' : isPromptOpen && 'hidden lg:block'
+                }`}
+              >
+                <div className='flex justify-center pt-6 lg:pt-0'>
+                  <div className='lg:absolute top-0 lg:top-14'>
+                    <ProfileImageUpload />
+                    <p className='text-xl font-normal text-center text-white mt-2'>
+                      Upload new photo
+                    </p>
+                  </div>
+                </div>
+                <div className='px-10 mt-14 lg:mt-44 lg:px-40'>
+                  <p className='text-base font-normal text-white'>Username</p>
+
+                  <div className='flex items-center justify-between mt-1'>
+                    <p className='text-lg text-white font-normal lg:hidden'>
+                      {getValues('username') ? getValues('username') : name}
+                    </p>
+
+                    <div className='hidden lg:flex lg:flex-col h-max w-full pr-8 mt-1'>
+                      <div className='flex items-center w-full relative'>
+                        <ProfileInput
+                          containerClass='w-full'
+                          disabled={disabledInput}
+                          placeholder={name}
+                          name='username'
+                          options={usernameOptions}
+                          inputClass='min-w-[362px] placeholder-custom-neutral-800'
+                        />
+                        <div
+                          className='text-lg font-normal absolute -right-14 text-custom-gray-300 ml-8 hidden lg:block'
+                          onClick={disabledHandler}
+                        >
+                          Edit
+                        </div>
+                      </div>
+
+                      <div className='h-5 text-left mb-10'>
+                        <ErrorMessage
+                          name='username'
+                          errors={errors}
+                          render={({ message }) => (
+                            <Message message={message} />
+                          )}
+                        />
+                      </div>
+                      <div className='border-b border-solid border-border-b' />
+                    </div>
+
+                    <div
+                      className='text-lg font-normal text-custom-gray-300 lg:hidden'
+                      onClick={() => setIsOpen(true)}
+                    >
+                      Edit
+                    </div>
+                  </div>
+
+                  <div className='border-b border-solid border-border-b mt-4 lg:hidden' />
+
+                  <p className='text-base font-normal text-white mt-8 lg:mt-10'>
+                    Email
+                  </p>
+
+                  <div className='pr-8 hidden lg:block'>
+                    <input
+                      placeholder={
+                        emails
+                          .map((user: { email: string }) => user.email)
+                          .toString()!
+                      }
+                      disabled={true}
+                      className={`text-black bg-custom-gray-300 mt-1 pl-4 pr-11 placeholder-custom-neutral-800 md:py-2 h-12 border-custom-gray-500 rounded w-full border focus:ring-0  text-base md:text-xl focus:outline-none focus:border-custom-gray-500  border-solid  outline-none font-normal`}
+                    />
+                  </div>
+                  <p className='text-lg text-white font-normal lg:hidden'>
+                    {
+                      emails
+                        .map((user: { email: string }) => user.email)
+                        .toString()!
+                    }
+                  </p>
+                  <div className='border-b border-solid border-border-b mt-4 lg:hidden' />
+                </div>
+              </div>
+              {saveChanges && !isOpen && !isPromptOpen && (
+                <div>
+                  <div className='flex items-center justify-between lg:justify-end mt-5 px-5'>
+                    <div
+                      className='text-white text-base font-normal cursor-pointer'
+                      onClick={clearInputs}
+                    >
+                      Cancel
+                    </div>
+                    <Button
+                      type='submit'
+                      className='bg-custom-red-600 hover:bg-custom-red-700 py-2 px-[0.4rem] rounded text-white cursor-pointer text-base font-normal lg:ml-6'
+                      onClick={confirmPrompt}
+                    >
+                      Save Changes
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </form>
+            <div className='lg:hidden'>
+              {isOpen && (
+                <div>
+                  <div className='w-full h-max px-10 bg-header-rgba pt-20 pb-[4.6rem] rounded-xl'>
+                    <ProfileInput
+                      label='Enter new username'
+                      name='username'
+                      options={usernameOptions}
+                    />
+                    <div className='mt-1 h-5'>
+                      <ErrorMessage
+                        name='username'
+                        errors={errors}
+                        render={({ message }) => <Message message={message} />}
+                      />
+                    </div>
+                  </div>
+                  <div className='flex items-center justify-between px-10 mt-10'>
+                    <div
+                      className='text-white text-base font-normal cursor-pointer'
+                      onClick={closeAddUser}
+                    >
+                      Cancel
+                    </div>
+                    <div
+                      className='bg-custom-red-600 hover:bg-custom-red-700 py-2 px-5 rounded text-white cursor-pointer text-base font-normal'
+                      onClick={checkUsernameHandler}
+                    >
+                      Add
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className='lg:hidden'>
+              {isPromptOpen && (
+                <div className='px-10'>
+                  <div className='relative'>
+                    <div className='bg-prompt-bg w-full -rotate-180 opacity-30 rounded-xl h-52' />
+                    <div className='absolute top-0 pt-16 w-full'>
+                      <div className='text-center w-full'>
+                        <p className='text-white'>
+                          Are you sure to make changes ?
+                        </p>
+                      </div>
+                      <div className='border-b border-solid border-border-b mt-11 w-full' />
+                      <div className='flex items-center justify-between mt-5 px-5'>
+                        <div
+                          className='text-white text-base font-normal cursor-pointer'
+                          onClick={closePrompt}
+                        >
+                          Cancel
+                        </div>
+                        <div
+                          className='bg-custom-red-600 hover:bg-custom-red-700 py-2 px-[0.4rem] rounded text-white cursor-pointer text-base font-normal'
+                          onClick={confirmPrompt}
+                        >
+                          Confirm
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </FormProvider>
+        </div>
+      </div>
+    </Dashboard>
+  );
+};
+
+export default GoogleProfile;
---
+++ components/Profile/Google/useGoogleProfile.tsx
@@ -0,0 +1,146 @@
+import { useGetUserData, useProfile } from 'hooks';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
+import { useMutation } from 'react-query';
+import { checkusername, updateGoogleProfile } from 'services';
+
+const useGoogleProfile = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isPromptOpen, setIsPromptOpen] = useState<boolean>(false);
+  const [saveChanges, setSaveChanges] = useState<boolean>(false);
+  const [showSuccess, setShowSuccess] = useState<boolean>(false);
+  const [disabledInput, setDisabledInput] = useState<boolean>(true);
+
+  useGetUserData();
+  const { name, id, avatarLoader, emails, image: avatarImage } = useProfile();
+
+  const { t } = useTranslation('forms');
+  const { push } = useRouter();
+
+  const form = useForm<{ username: string; image: string | File }>({
+    mode: 'all',
+    defaultValues: { username: name, image: avatarImage },
+  });
+
+  const {
+    formState: { errors },
+    setValue,
+    getValues,
+    setError,
+    control,
+    handleSubmit,
+  } = form;
+
+  useWatch({ control: control, name: ['image'] });
+  const newAvatar = getValues('image');
+
+  useEffect(() => {
+    if (typeof newAvatar !== 'string') {
+      setSaveChanges(true);
+    }
+  }, [newAvatar]);
+
+  const closeShowSuccessHandler = () => {
+    setShowSuccess(false);
+  };
+
+  const usernameOptions = {
+    minLength: { value: 3, message: t('errors.min') },
+    maxLength: { value: 15, message: t('errors.max') },
+    pattern: {
+      value: /^[a-z0-9]*$/,
+      message: t('errors.usernamePattern'),
+    },
+  };
+
+  const closeAddUser = () => {
+    setValue('username', '');
+    setIsOpen(false);
+  };
+
+  const closePrompt = () => {
+    setIsPromptOpen(false);
+    setIsOpen(true);
+  };
+
+  const confirmPrompt = () => {
+    setIsPromptOpen(false);
+    setSaveChanges(true);
+  };
+
+  const clearInputs = () => {
+    setValue('image', '');
+    setValue('username', '');
+    setSaveChanges(false);
+  };
+
+  const checkUsernameHandler = async () => {
+    const getUsername = getValues('username');
+    if (getUsername) {
+      try {
+        await checkusername({ username: getUsername, userId: +id });
+        setIsOpen(false);
+        setIsPromptOpen(true);
+      } catch (error: any) {
+        setError('username', { message: t('unique.name')! });
+      }
+    }
+  };
+
+  const { mutate } = useMutation(updateGoogleProfile, {
+    onSuccess: () => {
+      setSaveChanges(false);
+      setValue('username', '');
+      setValue('image', '');
+      setDisabledInput(true);
+      push('/profile');
+      setShowSuccess(true);
+    },
+    onError: () => {
+      setError('username', { message: t('unique.name')! });
+    },
+  });
+
+  const disabledHandler = () => {
+    setDisabledInput(false);
+    setSaveChanges(true);
+  };
+
+  const updateProfile = () => {
+    mutate({
+      username: getValues('username') ? getValues('username') : name,
+      userId: +id,
+      image: getValues('image') ? getValues('image') : avatarImage,
+    });
+  };
+
+  return {
+    avatarLoader,
+    name,
+    emails,
+    isOpen,
+    setIsOpen,
+    form,
+    usernameOptions,
+    errors,
+    closeAddUser,
+    checkUsernameHandler,
+    isPromptOpen,
+    closePrompt,
+    confirmPrompt,
+    getValues,
+    clearInputs,
+    saveChanges,
+    updateProfile,
+    handleSubmit,
+    showSuccess,
+    closeShowSuccessHandler,
+    disabledInput,
+    setDisabledInput,
+    disabledHandler,
+  };
+};
+
+export default useGoogleProfile;
