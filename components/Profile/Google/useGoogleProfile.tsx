5348ff4f6e7112d58f2f444ff96ce57602901b10
temito93 <temotemo161@gmail.com>
2023-02-10 02:14:56 +0400

    feat: add user profile with custom hook

--- components/Profile/Google/useGoogleProfile.tsx
+++ components/Profile/Google/useGoogleProfile.tsx
@@ -4,36 +4,88 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import { useMutation } from 'react-query';
-import { checkusername, updateGoogleProfile } from 'services';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  checkusername,
+  updateGoogleProfile,
+  updateProfilePassword,
+  updateUsername,
+} from 'services';
+import { showEmailActions } from 'store';
 
 const useGoogleProfile = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isPromptOpen, setIsPromptOpen] = useState<boolean>(false);
   const [saveChanges, setSaveChanges] = useState<boolean>(false);
   const [showSuccess, setShowSuccess] = useState<boolean>(false);
   const [disabledInput, setDisabledInput] = useState<boolean>(true);
+  const [disabledPassword, setDisabledPassword] = useState<boolean>(true);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+  const [nameUpdated, setNameUpdated] = useState<boolean>(false);
+  const [passwordUpdated, setPasswordUpdated] = useState<boolean>(false);
 
   useGetUserData();
+
+  const dispatch = useDispatch();
+
+  const emailModal = useSelector(
+    (state: {
+      email: {
+        modal: {
+          showAddEmailSuccess: boolean;
+          primaryChanged: boolean;
+          isEmailVerified: boolean;
+        };
+      };
+    }) => state.email.modal
+  );
+
+  const closeEmailAddSuccess = () => {
+    dispatch(showEmailActions.setShowAddEmailSuccess(false));
+  };
+
+  const closeEmailVerifiedSuccess = () => {
+    dispatch(showEmailActions.setIsEmailVerified(false));
+  };
+
+  const closePrimaryChangeSuccess = () => {
+    dispatch(showEmailActions.setPrimaryChanged(false));
+  };
+
   const { name, id, avatarLoader, emails, image: avatarImage } = useProfile();
 
   const { t } = useTranslation('forms');
-  const { push } = useRouter();
+  const { push, query } = useRouter();
 
-  const form = useForm<{ username: string; image: string | File }>({
+  const form = useForm<{
+    username: string;
+    image: string | File;
+    password?: string;
+    confirm_password?: string;
+  }>({
     mode: 'all',
-    defaultValues: { username: name, image: avatarImage },
+    defaultValues: {
+      username: name,
+      image: avatarImage,
+      password: '',
+      confirm_password: '',
+    },
   });
 
   const {
-    formState: { errors },
+    formState: { errors, isValid },
     setValue,
     getValues,
     setError,
     control,
     handleSubmit,
+    clearErrors,
   } = form;
 
-  useWatch({ control: control, name: ['image'] });
+  useWatch({
+    control: control,
+    name: ['image', 'password', 'confirm_password'],
+  });
   const newAvatar = getValues('image');
 
   useEffect(() => {
@@ -42,8 +94,28 @@ const useGoogleProfile = () => {
     }
   }, [newAvatar]);
 
+  const showPasswordhandler = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const showAddUserHandler = () => {
+    push('/profile?mode=edit-username');
+  };
+
+  const showConfirmPasswordHandler = () => {
+    setShowConfirmPassword((prev) => !prev);
+  };
+
   const closeShowSuccessHandler = () => {
     setShowSuccess(false);
+  };
+
+  const closeNameUpdated = () => {
+    setNameUpdated(false);
+  };
+
+  const closePasswordUpdated = () => {
+    setPasswordUpdated(false);
   };
 
   const usernameOptions = {
@@ -55,34 +127,87 @@ const useGoogleProfile = () => {
     },
   };
 
-  const closeAddUser = () => {
-    setValue('username', '');
-    setIsOpen(false);
+  const passwordOptions = {
+    minLength: { value: 8, message: t('errors.minPassword') },
+    maxLength: { value: 15, message: t('errors.max') },
+    pattern: {
+      value: /^[a-z0-9]*$/,
+      message: t('errors.passwordPattern'),
+    },
   };
 
-  const closePrompt = () => {
-    setIsPromptOpen(false);
-    setIsOpen(true);
+  const confirmPasswordOptions = {
+    validate: (value: string) => value == getValues('password'),
+  };
+
+  const checkPasswordHandler = () => {
+    if (!getValues('password')) {
+      return setError('password', { message: t('errors.required')! });
+    }
+    getValues('password') !== getValues('confirm_password')
+      ? setError('password', { message: t('errors.confirmPassword')! })
+      : push('/profile?mode=password-confirm');
+    setSaveChanges(true);
+  };
+
+  const closeAddUser = () => {
+    clearErrors('username');
+    setValue('username', '');
+    push('/profile');
+  };
+
+  const closeAddPassword = () => {
+    clearErrors('password');
+    clearErrors('confirm_password');
+    setValue('password', '');
+    setValue('confirm_password', '');
+    push('/profile');
+  };
+
+  const closeUserNamePrompt = () => {
+    push('/profile?mode=edit-username');
+  };
+
+  const closePasswordPrompt = () => {
+    push('/profile?mode=edit-password');
   };
 
   const confirmPrompt = () => {
-    setIsPromptOpen(false);
     setSaveChanges(true);
+    push('/profile');
   };
 
   const clearInputs = () => {
     setValue('image', '');
     setValue('username', '');
+    setValue('password', '');
+    setValue('confirm_password', '');
     setSaveChanges(false);
+    setDisabledInput(true);
+    setDisabledPassword(true);
+    clearErrors('username');
+    clearErrors('password');
+    push('/profile');
+  };
+
+  const passwordOpenHandler = () => {
+    push('/profile?mode=edit-password');
+  };
+
+  const desktopPasswordOpenHandler = () => {
+    setSaveChanges(true);
+    push('/profile?mode=edit-password');
   };
 
   const checkUsernameHandler = async () => {
     const getUsername = getValues('username');
-    if (getUsername) {
+    if (!getUsername) {
+      return setError('username', { message: t('errors.required')! });
+    }
+    if (getUsername && isValid) {
       try {
         await checkusername({ username: getUsername, userId: +id });
-        setIsOpen(false);
-        setIsPromptOpen(true);
+        push('/profile?mode=username-confirm');
       } catch (error: any) {
         setError('username', { message: t('unique.name')! });
       }
@@ -91,20 +216,57 @@ const useGoogleProfile = () => {
 
   const { mutate } = useMutation(updateGoogleProfile, {
     onSuccess: () => {
-      setSaveChanges(false);
       setValue('username', '');
       setValue('image', '');
+      setValue('password', '');
+      setValue('confirm_password', '');
       setDisabledInput(true);
-      push('/profile');
       setShowSuccess(true);
+      setSaveChanges(false);
     },
     onError: () => {
       setError('username', { message: t('unique.name')! });
     },
   });
 
+  const { mutate: updateUsernameInstance } = useMutation(updateUsername, {
+    onSuccess: () => {
+      setValue('username', '');
+      setNameUpdated(true);
+      push('/profile');
+    },
+  });
+
+  const { mutate: updatePasswordInstance } = useMutation(
+    updateProfilePassword,
+    {
+      onSuccess: () => {
+        setValue('password', '');
+        setValue('confirm_password', '');
+        setPasswordUpdated(true);
+        push('/profile');
+      },
+    }
+  );
+
+  const updateUsernameHandler = () => {
+    updateUsernameInstance({ username: getValues('username') });
+  };
+
+  const updatePasswordHandler = () => {
+    updatePasswordInstance({
+      password: getValues('password')?.toString()!,
+      confirm_password: getValues('confirm_password')?.toString()!,
+    });
+  };
+
   const disabledHandler = () => {
     setDisabledInput(false);
+    setSaveChanges(true);
+  };
+
+  const disabledPasswordHandler = () => {
+    setDisabledPassword(false);
     setSaveChanges(true);
   };
 
@@ -113,6 +275,7 @@ const useGoogleProfile = () => {
       username: getValues('username') ? getValues('username') : name,
       userId: +id,
       image: getValues('image') ? getValues('image') : avatarImage,
+      password: getValues('password') ? getValues('password') : '',
     });
   };
 
@@ -120,15 +283,12 @@ const useGoogleProfile = () => {
     avatarLoader,
     name,
     emails,
-    isOpen,
-    setIsOpen,
     form,
     usernameOptions,
     errors,
     closeAddUser,
     checkUsernameHandler,
-    isPromptOpen,
-    closePrompt,
+    closeUserNamePrompt,
     confirmPrompt,
     getValues,
     clearInputs,
@@ -141,6 +301,32 @@ const useGoogleProfile = () => {
     setDisabledInput,
     disabledHandler,
     t,
+    passwordOptions,
+    closeAddPassword,
+    checkPasswordHandler,
+    confirmPasswordOptions,
+    disabledPasswordHandler,
+    disabledPassword,
+    passwordOpenHandler,
+    showConfirmPasswordHandler,
+    showPasswordhandler,
+    showPassword,
+    showConfirmPassword,
+    desktopPasswordOpenHandler,
+    emailModal,
+    closeEmailAddSuccess,
+    closePrimaryChangeSuccess,
+    query,
+    isValid,
+    showAddUserHandler,
+    closePasswordPrompt,
+    updateUsernameHandler,
+    nameUpdated,
+    closeNameUpdated,
+    updatePasswordHandler,
+    passwordUpdated,
+    closePasswordUpdated,
+    closeEmailVerifiedSuccess,
   };
 };
 
