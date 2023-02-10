fb3ebd836d22925f181dc5b87e2e60e40204fc9b
temito93 <temotemo161@gmail.com>
2023-02-10 14:31:04 +0400

    chore: remove ?_method=put from updateUsername instance

--- services/Profile/updateUsername.ts
+++ services/Profile/updateUsername.ts
@@ -1,7 +1,7 @@
 import instance from 'services/axios';
 
 const updateUsername = (data: { username: string }) => {
-  return instance.post(`/api/auth/update-username/?_method=put`, data);
+  return instance.put(`/api/auth/update-username`, data);
 };
 
 export default updateUsername;
