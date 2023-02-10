b50bd01c78fcb967150811a781976298bc9c3187
temito93 <temotemo161@gmail.com>
2023-02-10 17:33:14 +0400

    chore: add global type for pusher

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,11 @@
+import Pusher from 'pusher';
+
 export * from './modal';
 export * from './user';
 export * from './movie';
+
+declare global {
+  interface Window {
+    pusher: typeof Pusher;
+  }
+}
