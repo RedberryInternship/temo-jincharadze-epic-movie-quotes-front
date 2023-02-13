24d480c4b7b026a64b61738dc42a7684cb41e26e
temito93 <temotemo161@gmail.com>
2023-02-13 19:25:05 +0400

    chore: add routes in middleware to run this function only if this routes are available

--- components/icons/Ghost.tsx
+++ components/icons/Ghost.tsx
@@ -7,7 +7,7 @@ const Ghost = () => {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <g clip-path='url(#clip0_346_43032)'>
+      <g clipPath='url(#clip0_346_43032)'>
         <path
           fillRule='evenodd'
           clipRule='evenodd'
--- pages/404.tsx
+++ pages/404.tsx
@@ -10,14 +10,14 @@ const Index = () => {
     <div className='bg-top-gradient h-screen w-full flex flex-col justify-center items-center'>
       <Ghost />
       <h2 className='text-2xl text-white font-bold mt-9 md:text-47'>
-        {t('notFound.title')}
+        {t('notFound.title')!}
       </h2>
       <p className='text-base md:text-2xl font-medium text-center text-white mt-4 max-w-[18.5rem] md:max-w-none'>
-        {t('notFound.description')}
+        {t('notFound.description')!}
       </p>
       <Link href='/' className='mt-8'>
         <div className='w-[9.4rem] h-10 md:h-12 text-base md:text-xl bg-custom-red-600 hover:bg-custom-red-700 flex items-center justify-center text-white rounded font-normal '>
-          {t('notFound.btn')}
+          {t('notFound.btn')!}
         </div>
       </Link>
     </div>
